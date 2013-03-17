10c959576453d57026a6d90a6b94a8a56c53cbdb
Dan Weatbrook <dweatbrook@expedia.com>
2013-03-17 12:55:41 -0700

    adding a item to c3.txt and going to promote

--- nc.rb
+++ nc.rb
@@ -1,6 +1,8 @@
 puts "in my riby script"
 status = `git status`
-puts "status is >> #{status} <<"
+branch = /# On Branch (\w+)/.match(status)
+puts "Branch is #{branch}"
+
 puts `echo a random string #{rand(100)} >> c3.txt`
 puts `git commit -a -m "adding a item to c3.txt and going to promote"`
 puts `git push origin dev`
