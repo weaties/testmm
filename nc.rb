ba43ea62ee1030321693181a74693fdfbd2a859c
Dan Weatbrook <dweatbrook@expedia.com>
2013-03-17 12:44:31 -0700

    adding a item to c3.txt and going to promote

--- nc.rb
+++ nc.rb
@@ -1,4 +1,4 @@
 puts "in my riby script"
 puts `echo a random string #{rand(100)} >> c3.txt`
 puts `git commit -a -m "adding a item to c3.txt and going to promote"`
-puts `git push origin`
+puts `git push origin dev`
