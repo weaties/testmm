dce685d691d42d19662ca0d304f8428396c559bf
Dan Weatbrook <dweatbrook@expedia.com>
2013-03-17 13:17:24 -0700

    adding a item to trunk.txt on branch trunk and going to promote

--- nc.rb
+++ nc.rb
@@ -5,5 +5,6 @@ branch = status.match(/On branch (\w+)/)[1]
 puts "Branch is #{branch}"
 
 puts `echo a random string #{rand(100)} >> #{branch}.txt`
+puts `git add #{branch}.txt`
 puts `git commit -a -m "adding a item to #{branch}.txt on branch #{branch} and going to promote"`
 puts `git push origin #{branch}`
